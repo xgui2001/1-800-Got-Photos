069ea28fdcc2bbb0db30cd90e99c2cd97b91c110
24ag24 <ag24@williams.edu>
2022-11-27 23:47:17 -0500

    wip

--- code/Parser.fs
+++ code/Parser.fs
@@ -5,12 +5,26 @@ open System
 open SixLabors.ImageSharp
 open SixLabors.ImageSharp.Drawing
 
+// constructs an Expr type; it could be a Variable of Image type (for now)
 type Expr =
     | Variable of Image
-    | Application of Image * Expr
+    //| Application of Image * Expr <-- will implement later
 
+// a declaration for a parser called expr and an implementation for that same parser called exprImpl.
+// takes in an Input type and returns Outcome <Expr> type.
 let expr, exprImpl = recparser ()
 
-let variable: Parser<Expr>
+// a declaration for a variable parser that parses in a char and returns a Variable. Return type is of Parser<Expr>
+let variable: Parser<Expr> = use image.Load(Cat03.jpeg) //(fun a:Image -> Variable (a))
+
+let parse input : Expr option =
+    match grammar (prepare input) with
+    | Success (ws, _) -> Some ws
+    | Failure (_, _) -> None
+
+
+// a declaration for a grammar parser that takes in an expr and checks whether it has reached the end of the input. Return type is of Parser<Expr>
+let grammar = pleft expr peof
+
 // an implementation for the expr parser that determines whether to use the variable parser, the abstraction parser or the application parser.
 exprImpl := (variable) <|> (application)
--- code/Program.fs
+++ code/Program.fs
@@ -1,5 +1,5 @@
-﻿open Parser
-open Evaluator
+﻿//open Parser
+//open Evaluator
 
 open System.IO
 open SixLabors.ImageSharp
@@ -7,6 +7,10 @@ open SixLabors.ImageSharp.Drawing
 
 [<EntryPoint>]
 let main args =
-if args.Length < int args.[0] then
-    printfn "Usage: dotnet run <filename>"
-  else
+    if args.Length < 1 then
+        printfn "Usage: dotnet run <filename>"
+    else
+        let picture = use image = Image.Load(args.[0])
+        image.Save(args.[0] + ".jpg")
+
+    0
